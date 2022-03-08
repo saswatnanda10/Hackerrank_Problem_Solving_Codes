dc027045e330c516d505ef06aa845e83c95d4f70
saswatnanda10 <67865173+saswatnanda10@users.noreply.github.com>
2022-03-08 08:23:24 +0530

    Add files via upload
    There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
    
    The elements of the first array are all factors of the integer being considered
    The integer being considered is a factor of all elements of the second array
    These numbers are referred to as being between the two arrays. Determine how many such numbers exist.

---
+++ between_2_sets.py
@@ -0,0 +1,37 @@
+'''There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+
+The elements of the first array are all factors of the integer being considered
+The integer being considered is a factor of all elements of the second array
+These numbers are referred to as being between the two arrays. Determine how many such numbers exist.'''
+
+def getTotalX(a, b):
+    # Write your code here
+    ans=0
+    for i in range(1, 101):
+        if all(i%x==0 for x in a) and all(x%i==0 for x in b):
+            ans+=1
+            
+    return ans
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
+
+
+
